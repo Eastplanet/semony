1eaf6ec5d8cdd5d69b0981a0b7e4c1fb3e373020
박지훈 <zlxlgus123@naver.com>
2024-10-29 11:47:12 +0900

    Test : LoggingServiceImplTest 테스트 추가

---
+++ maker/src/test/java/com/semony/maker/LoggingServiceImplTest.java
@@ -0,0 +1,84 @@
+package com.semony.maker;
+
+import static org.mockito.Mockito.verify;
+
+import com.semony.maker.application.service.LoggingServiceImpl;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+
+class LoggingServiceImplTest {
+
+    @Mock
+    private MongoTemplate mongoTemplate;
+
+    @InjectMocks
+    private LoggingServiceImpl loggingService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testLogError() {
+        // Given
+        String message = "Test error message";
+        String recipe = "Test recipe";
+        String moduleName = "TestModule";
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDate requestTime = LocalDate.now();
+        String lotId = "LP22024100315_PJ2@89654577";
+        long lotSeq = 123456;
+
+        // When
+        loggingService.logError(message, recipe, moduleName, currentTime, requestTime, lotId,
+            lotSeq);
+
+        // Then
+        verify(mongoTemplate).save(Map.of(
+            "status", "error",
+            "message", message,
+            "recipe", recipe,
+            "module", moduleName,
+            "current_time", currentTime,
+            "request_time", requestTime,
+            "lot_id", lotId,
+            "lot_seq", lotSeq
+        ), "inspectionLogs");
+    }
+
+    @Test
+    void testLogSuccess() {
+        // Given
+        String message = "Test success message";
+        String recipe = "Test recipe";
+        String moduleName = "TestModule";
+        LocalDateTime currentTime = LocalDateTime.now();
+        LocalDate requestTime = LocalDate.now();
+        String lotId = "LP22024100315_PJ2@89654577";
+        long lotSeq = 123456;
+
+        // When
+        loggingService.logSuccess(message, recipe, moduleName, currentTime, requestTime, lotId,
+            lotSeq);
+
+        // Then
+        verify(mongoTemplate).save(Map.of(
+            "status", "success",
+            "message", message,
+            "recipe", recipe,
+            "module", moduleName,
+            "current_time", currentTime,
+            "request_time", requestTime,
+            "lot_id", lotId,
+            "lot_seq", lotSeq
+        ), "inspectionLogs");
+    }
+}
