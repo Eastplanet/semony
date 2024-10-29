70cf2f69e242e4d93d42b344b8c890c18805b4aa
박지훈 <zlxlgus123@naver.com>
2024-10-29 16:12:53 +0900

    Refactor : self-invocation 문제 해결

--- maker/src/main/java/com/semony/maker/application/service/LotServiceImpl.java
+++ maker/src/main/java/com/semony/maker/application/service/LotServiceImpl.java
@@ -6,51 +6,42 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class LotServiceImpl implements LotService {
 
     private final LotMetadataRepository lotMetadataRepository;
+    private final LotTransactionServiceImpl lotTransactionServiceImpl;
 
+    /**
+     * LotId를 가져오고 새로 갱신합니다.
+     * @return
+     */
     @Override
     public String generateLotId() {
         LotMetadata metadata = lotMetadataRepository.findTopByOrderByLastLotIdDesc();
 
-        // lotId 생성
         Long newLastLotId = metadata.getLastLotId() + 1;
         String timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyMMddHHmm"));
         String lotId = "LP2" + timestamp + "_PJ2@" + newLastLotId;
 
-        // lastLotId 업데이트
-        updateLastLotId(newLastLotId);
+        lotTransactionServiceImpl.updateLastLotId(newLastLotId);
 
         return lotId;
     }
 
+    /**
+     * LotSeq를 가져오고 새로 갱신합니다.
+     * @return
+     */
     @Override
     public Long generateLotSeq() {
         LotMetadata metadata = lotMetadataRepository.findTopByOrderByLastLotSeqDesc();
 
-        // lastLotSeq 업데이트 및 반환
         Long newLastLotSeq = metadata.getLastLotSeq() + 1;
-        updateLastLotSeq(newLastLotSeq);
+        lotTransactionServiceImpl.updateLastLotSeq(newLastLotSeq);
 
         return newLastLotSeq;
-    }
-
-    @Transactional
-    public void updateLastLotId(Long newLastLotId) {
-        LotMetadata metadata = lotMetadataRepository.findTopByOrderByLastLotIdDesc();
-        metadata.setLastLotId(newLastLotId);
-        lotMetadataRepository.save(metadata);
-    }
-
-    @Transactional
-    public void updateLastLotSeq(Long newLastLotSeq) {
-        LotMetadata metadata = lotMetadataRepository.findTopByOrderByLastLotSeqDesc();
-        metadata.setLastLotSeq(newLastLotSeq);
-        lotMetadataRepository.save(metadata);
     }
 }
