13742ad8e8d83e037b64327963cd699995317bfd
박지훈 <zlxlgus123@naver.com>
2024-10-29 15:42:29 +0900

    Feat : EqpInspectionHstAlpha 클래스 추가

---
+++ maker/src/main/java/com/semony/maker/domain/entity/EqpInspectionHstAlpha.java
@@ -0,0 +1,78 @@
+package com.semony.maker.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 요구사항: 기본 생성자는 protected로 선언
+@Table(name = "eqp_inspection_hst_alpha")
+public class EqpInspectionHstAlpha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_eqp_inspection_hst_alpha")
+    @SequenceGenerator(name = "seq_eqp_inspection_hst_alpha", sequenceName = "public.seq_eqp_inspection_hst_alpha", allocationSize = 1)
+    private Long rawid;
+
+    @Column(name = "event_dtts", nullable = false)
+    private LocalDateTime eventDtts;
+
+    @Column(name = "module_id", nullable = false, length = 32)
+    private String moduleId;
+
+    @Column(name = "lot_id", length = 32)
+    private String lotId;
+
+    @Column(name = "lot_seq", precision = 20, scale = 0)
+    private BigDecimal lotSeq;
+
+    @Column(name = "slot_no", length = 5)
+    private String slotNo;
+
+    @Column(name = "flow_recipe", length = 256)
+    private String flowRecipe;
+
+    @Column(name = "process_recipe", length = 32)
+    private String processRecipe;
+
+    @Column(name = "port_no", length = 5)
+    private String portNo;
+
+    @Column(name = "defect_count")
+    private Long defectCount;
+
+    @Column(name = "create_dtts")
+    private LocalDateTime createDtts;
+
+    @Column(name = "create_by", length = 32)
+    private String createBy;
+
+    public static EqpInspectionHstAlpha create(LocalDateTime eventDtts, String moduleId,
+        String lotId, BigDecimal lotSeq,
+        String slotNo, String flowRecipe, String processRecipe, String portNo,
+        Long defectCount, LocalDateTime createDtts, String createBy) {
+        EqpInspectionHstAlpha instance = new EqpInspectionHstAlpha();
+        instance.eventDtts = eventDtts;
+        instance.moduleId = moduleId;
+        instance.lotId = lotId;
+        instance.lotSeq = lotSeq;
+        instance.slotNo = slotNo;
+        instance.flowRecipe = flowRecipe;
+        instance.processRecipe = processRecipe;
+        instance.portNo = portNo;
+        instance.defectCount = defectCount;
+        instance.createDtts = createDtts;
+        instance.createBy = createBy;
+        return instance;
+    }
+}
