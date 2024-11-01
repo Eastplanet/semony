28ce28290c66df26eff775611c9ecbc0995522eb
정희수 <s007kk@ajou.ac.kr>
2024-11-01 12:13:06 +0900

    Feat: ebr 조회 화면 구현

--- semony_fe/src/app/(route)/detail/ebr/page.tsx
+++ semony_fe/src/app/(route)/detail/ebr/page.tsx
@@ -1,11 +1,17 @@
 'use client'
-
+import Image from 'next/image';
 const DetailPage: React.FC = () => {
 
   return (
-    <div>
-     ebr
-    </div>
+      <div className="rounded-3xl shadow-lg w-[100%] flex items-center justify-center h-[80%] overflow-hidden">
+        <Image
+          src="/mocks/ebr/EBR_RESULT.BMP"
+          alt="ebr"
+          width={2000}
+          height={1000}
+          className="object-center"
+        />
+      </div>
   );
 };
 
