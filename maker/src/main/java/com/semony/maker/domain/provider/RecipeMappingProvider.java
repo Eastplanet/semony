797c910df910f148c53f8d9cdbf1cd51f731febd
박지훈 <zlxlgus123@naver.com>
2024-11-04 17:48:33 +0900

    Fix : 모듈명 변경

--- maker/src/main/java/com/semony/maker/domain/provider/RecipeMappingProvider.java
+++ maker/src/main/java/com/semony/maker/domain/provider/RecipeMappingProvider.java
@@ -13,37 +13,37 @@ public class RecipeMappingProvider {
 
         // 16가지 고정된 조합을 미리 매핑
         recipeMap.put("0TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM1-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM1-36"));
         recipeMap.put("1TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM1-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM1-46"));
         recipeMap.put("2TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM2-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM2-36"));
         recipeMap.put("3TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM2-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM2-46"));
         recipeMap.put("4TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM1-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM1-36"));
         recipeMap.put("5TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM1-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM1-46"));
         recipeMap.put("6TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM2-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM2-36"));
         recipeMap.put("7TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM2-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM2-46"));
         recipeMap.put("8TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM1-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM1-36"));
         recipeMap.put("9TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM1-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM1-46"));
         recipeMap.put("10TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM2-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM2-36"));
         recipeMap.put("11TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM2-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM2-46"));
         recipeMap.put("12TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM1-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM1-36"));
         recipeMap.put("13TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM1-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM1-46"));
         recipeMap.put("14TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-51", "MIW-7-61", "EWIM2-36"));
+            new RecipeCombination("MIW7-51", "MIW7-61", "EWIM2-36"));
         recipeMap.put("15TT_EWIM_NO_CHHP",
-            new RecipeCombination("MIW-7-52", "MIW-7-62", "EWIM2-46"));
+            new RecipeCombination("MIW7-52", "MIW7-62", "EWIM2-46"));
     }
 
     public RecipeCombination generateCombination(String recipe) {
