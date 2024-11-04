22215ccdd674994802244ed7991a9a0cd3acccf9
박지훈 <zlxlgus123@naver.com>
2024-11-04 17:51:36 +0900

    Fix : sendModuleRequest 인터페이스 수정

--- maker/src/main/java/com/semony/maker/application/service/ModuleRequestService.java
+++ maker/src/main/java/com/semony/maker/application/service/ModuleRequestService.java
@@ -4,5 +4,7 @@ import java.time.LocalDate;
 
 public interface ModuleRequestService {
 
-    public void sendModuleRequest(String moduleName, LocalDate time);
+    public void sendModuleRequest(String moduleName, LocalDate date, String lotId,
+        String flowRecipe,
+        long lotSeq, int slotNo, String localFolderPath, String macroFolder, String selectModule);
 }
