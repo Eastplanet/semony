54abea828b2bdf21dc922ae91a9c933afc5aa5b4
박지훈 <zlxlgus123@naver.com>
2024-11-05 12:21:13 +0900

    Refactor : 비즈니스 예외처리 message 삭제

--- maker/src/main/java/com/semony/maker/global/error/exception/BusinessException.java
+++ maker/src/main/java/com/semony/maker/global/error/exception/BusinessException.java
@@ -10,13 +10,11 @@ public class BusinessException extends RuntimeException {
     private final String invalidValue;
     private final String fieldName;
     private final HttpStatus httpStatus;
-    private final String message;
 
     public BusinessException(Object invalidValue, String fieldName, ErrorCode errorCode) {
         super(errorCode.getMessage());
         this.invalidValue = invalidValue != null ? invalidValue.toString() : null;
         this.fieldName = fieldName;
         this.httpStatus = errorCode.getHttpStatus();
-        this.message = errorCode.getMessage();
     }
 }
