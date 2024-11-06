013b0c6fb3edcd193dc9f73c9b672c555fbd3b83
오동규 <ehdrb700606@naver.com>
2024-11-06 17:20:31 +0900

    Feat: waferDetail 조회 기능

--- integrated/src/main/java/com/semony/integrated/application/WaferService.java
+++ integrated/src/main/java/com/semony/integrated/application/WaferService.java
@@ -1,6 +1,9 @@
 package com.semony.integrated.application;
 
 import com.semony.integrated.domain.dto.SummaryWaferDto;
+import com.semony.integrated.domain.dto.WaferDetailDTO;
+import com.semony.integrated.domain.dto.smf.WaferInspectionDTO;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -11,4 +14,7 @@ public interface WaferService {
         String slotNo);
 
     public List<SummaryWaferDto> getWaferSummaryList(LocalDateTime startDate, LocalDateTime endDate);
+
+    public WaferDetailDTO getWaferDetail(String lotId, BigDecimal lotSeq, String flowRecipe, String slotNo)
+        throws IOException;
 }
