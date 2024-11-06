9d656d19c8c0f986429b1c66caf6cd4a479c669a
정희수 <s007kk@ajou.ac.kr>
2024-11-06 17:27:15 +0900

    Feat: 전역 웨이퍼 데이터 context 구현

--- semony_fe/src/app/(route)/detail/DataContext.tsx
+++ semony_fe/src/app/(route)/detail/DataContext.tsx
@@ -21,24 +21,24 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
   const [defectRecordsStep2, setDefectRecordsStep2] = useState<DefectRecord[]>([]);
   const [defectRecordsStep3, setDefectRecordsStep3] = useState<DefectRecord[]>([]);
 //   const [imagesData, setImagesData] = useState(null);
-
+  const fetchData = () => {
+    request(`wafer/detail?ppid=0TT_EWIM_NO_CHHP&lotId=LP22024100315_PJ2@89654577&lotSeq=727939436&slotNo=24`).then((data) => {
+      setDieLocations(data.DieLocations || []);
+      setDefectRecordsStep1(data.waferInspections[0].defectRecordSpec || []);
+      setDefectRecordsStep2(data.waferInspections[1].defectRecordSpec || []);
+      setDefectRecordsStep3(data.waferInspections[2].defectRecordSpec || []);
+    });
+  }
   useEffect(() => {
     // 첫 번째 API 요청: Wafer Map 데이터
-    axios.get("https://192.168.30.204/data")
-    .then((res)=> {
-        setDieLocations(res.data.DieLocations || []);
-        setDefectRecordsStep1(res.data.DefectRecordSpec || []);
-        setDefectRecordsStep2(res.data.DefectRecordSpec || []);
-        setDefectRecordsStep3(res.data.DefectRecordSpec || []);
-    })
-    .catch((err) => console.error(err));
+    fetchData();
 
     // 두 번째 API 요청: Images 데이터 (Lazy Loading)
     setTimeout(() => {
       request('images')
         .then((data) => console.log(data))
         .catch((err) => console.error(err));
-    }, 3000); // 3초 지연 후 요청
+    }, 1000); // 3초 지연 후 요청
   }, []);
 
   return (
