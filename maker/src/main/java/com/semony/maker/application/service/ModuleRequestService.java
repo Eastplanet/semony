89a801060adbb3402575a93ccd1e78a2cc024cae
박지훈 <zlxlgus123@naver.com>
2024-11-07 10:47:46 +0900

    Feat : 인터페이스 수정

--- maker/src/main/java/com/semony/maker/application/service/ModuleRequestService.java
+++ maker/src/main/java/com/semony/maker/application/service/ModuleRequestService.java
@@ -1,10 +1,11 @@
 package com.semony.maker.application.service;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 public interface ModuleRequestService {
 
-    public void sendModuleRequest(String moduleName, LocalDate date, String lotId,
+    Map<String, Long> sendModuleRequest(String moduleName, LocalDate date, String lotId,
         String flowRecipe,
         long lotSeq, int slotNo, String localFolderPath, String macroFolder, String selectModule);
 }
