9f4e6e39034edf8f1558a345d8120317888499a2
박지훈 <zlxlgus123@naver.com>
2024-11-08 10:08:35 +0900

    Feat : lot 단위로 만들기

--- maker/src/main/java/com/semony/maker/presentation/MakerController.java
+++ maker/src/main/java/com/semony/maker/presentation/MakerController.java
@@ -1,6 +1,9 @@
 package com.semony.maker.presentation;
 
+import static com.semony.maker.global.constants.Constants.MAX_SLOT_COUNT;
+
 import com.semony.maker.application.processor.InspectionProcessor;
+import com.semony.maker.application.service.LotService;
 import com.semony.maker.global.success.SuccessMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -16,10 +19,11 @@ import java.time.LocalDate;
 public class MakerController {
 
     private final InspectionProcessor inspectionProcessor;
-
+    private final LotService lotService;
     @Autowired
-    public MakerController(InspectionProcessor inspectionProcessor) {
+    public MakerController(InspectionProcessor inspectionProcessor , LotService lotService) {
         this.inspectionProcessor = inspectionProcessor;
+        this.lotService = lotService;
     }
 
     @PostMapping("/make")
@@ -29,4 +33,27 @@ public class MakerController {
         inspectionProcessor.processInspection(recipe, requestTime, slotId);
         return ResponseEntity.ok(SuccessMessage.INSPECTION_DATA_CREATION_SUCCESS + recipe);
     }
+
+    @PostMapping("/make/lot")
+    public ResponseEntity<?> makeInspectionData(@RequestParam String recipe,
+        @RequestParam LocalDate requestTime) {
+        String lotId = lotService.generateLotId();
+        long lotSeq = lotService.generateLotSeq();
+        for(int i = 0;i<MAX_SLOT_COUNT;i++){
+            inspectionProcessor.processInspection(recipe, requestTime, i, lotId, lotSeq);
+        }
+        return ResponseEntity.ok(SuccessMessage.INSPECTION_DATA_CREATION_SUCCESS + recipe);
+    }
+
+    @PostMapping("/make/test")
+    public ResponseEntity<?> makeInspectionData(@RequestParam LocalDate requestTime) {
+        for (int j = 0; j < 16; j++) {
+            String recipeKey = j + "TT_EWIM_NO_CHHP";
+            for (int i = 0; i < MAX_SLOT_COUNT; i++) {
+                inspectionProcessor.processInspection(recipeKey, requestTime, i);
+            }
+        }
+        return ResponseEntity.ok(SuccessMessage.INSPECTION_DATA_CREATION_SUCCESS);
+    }
+
 }
