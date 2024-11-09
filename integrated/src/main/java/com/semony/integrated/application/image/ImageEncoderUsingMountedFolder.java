071881efc1837347fd31586fe0ff8c1dd7dc139e
오동규 <ehdrb700606@naver.com>
2024-11-09 19:48:04 +0900

    Feat: 실제 NFS 폴더에 접근하여 ImageEncode 하는 기능 추가

--- integrated/src/main/java/com/semony/integrated/application/image/ImageEncoderUsingMountedFolder.java
+++ integrated/src/main/java/com/semony/integrated/application/image/ImageEncoderUsingMountedFolder.java
@@ -1,12 +1,169 @@
 package com.semony.integrated.application.image;
 
+import com.semony.integrated.application.PathFinder;
+import com.semony.integrated.domain.dto.image.IPU;
+import com.semony.integrated.domain.dto.image.ImageData;
 import com.semony.integrated.domain.dto.image.ImageSet;
+import com.semony.integrated.domain.entity.FlowRecipe;
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ImageEncoderUsingMountedFolder implements ImageEncoder {
 
     @Override
-    public ImageSet encode(String lotId, BigDecimal lotSeq, String flowRecipe, String slotNo) {
+    public List<ImageSet> encode(String lotId, BigDecimal lotSeq, String flowRecipe, String slotNo,
+        LocalDateTime date) {
+
+        PathFinder pathFinder = new PathFinder(FlowRecipe.findByPpid(flowRecipe), lotSeq.toString(),
+            lotId, slotNo, date);
+
+//        FileSystemResource resource = new FileSystemResource("../../var/libs/data/nfs_shared/MIW7-51 (IN)/InVision/SaveData/20241003/LP22024100315_PJ2@89654577[0TT_EWIM_NO_CHHP]-727939436/018/Macro[Inspection]/IPU_Patch");
+//
+//        if (!resource.exists()) {
+//            throw new RuntimeException("폴더를 찾을 수 없습니다: " + resource);
+//        }
+
+//        System.out.println(Arrays.stream(resource.getFile().listFiles()).count());
+
+        List<ImageSet> imageSets = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+
+            ImageSet imageSet = new ImageSet();
+
+            String path = null;
+            String ebrPath = null;
+
+            switch (i) {
+                case 0:
+                    path = pathFinder.getInPath();
+                    break;
+                case 1:
+                    path = pathFinder.getEwimPath();
+                    ebrPath = pathFinder.getEwimPathEBR();
+                    break;
+                case 2:
+                    path = pathFinder.getOutPath();
+                    break;
+            }
+
+            // Golden 가져오기
+            ImageData imageData = new ImageData();
+            imageData.setFileName("Golden");
+            imageData.setData(getFile(path, "Golden.jpg"));
+            imageSet.setGolden(imageData);
+
+            // Macro 가져오기
+            imageData = new ImageData();
+            imageData.setFileName("Macro");
+            imageData.setData(getFile(path, "PPID"));
+            imageSet.setMacro(imageData);
+
+            // IPU 가져오기
+            List<IPU> ipuList = new ArrayList<IPU>();
+            Map<String, List<ImageData>> groupData = getIPU(path);
+            for (Map.Entry<String, List<ImageData>> entry : groupData.entrySet()) {
+                IPU ipu = new IPU();
+                ipu.setIpuNum(Integer.parseInt(entry.getKey()));
+                ipu.setImages(entry.getValue());
+                ipuList.add(ipu);
+            }
+            imageSet.setIpus(ipuList);
+
+            // i==1 EWIM 모듈인 경우 EBR 가져오기
+            if(i==1){
+                imageData = new ImageData();
+                imageData.setFileName("EBR");
+                imageData.setData(getFile(ebrPath, "EBR"));
+                imageSet.setEBR(imageData);
+            }
+            imageSets.add(imageSet);
+        }
+
+        return imageSets;
+    }
+
+    private String getFile(String base, String findFile) {
+
+        FileSystemResource resource = new FileSystemResource(base);
+
+        if (!resource.exists()) {
+            throw new RuntimeException("폴더를 찾을 수 없습니다: " + base);
+        }
+
+        File[] files = resource.getFile().listFiles();
+
+        for (File file : files) {
+            if (file.getName().contains(findFile)) {
+                try {
+                    byte[] bytes = Files.readAllBytes(file.toPath());
+                    return Base64.getEncoder().encodeToString(bytes);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                break;
+            }
+        }
+
         return null;
     }
+
+
+
+    private Map<String, List<ImageData>> getIPU(String base) {
+
+        base = base + "/IPU_Patch";
+        FileSystemResource resource = new FileSystemResource(base);
+
+        if (!resource.exists()) {
+            throw new RuntimeException("폴더를 찾을 수 없습니다: " + base);
+        }
+
+        File[] files = resource.getFile().listFiles();
+
+        Map<String, List<ImageData>> groupData = new HashMap<>();
+
+        for (File file : files) {
+
+            try {
+                String fileNameWithoutExtension = file.getName().split("\\.")[0];
+//                System.out.println(file.toPath());
+                String data = Base64.getEncoder().encodeToString(Files.readAllBytes(file.toPath()));
+
+                ImageData imageData = new ImageData();
+                imageData.setFileName(fileNameWithoutExtension);
+                imageData.setData(data);
+
+                String[] num = fileNameWithoutExtension.split("_");
+                String group = num[0];
+
+                if (groupData.containsKey(group)) {
+                    groupData.get(group).add(imageData);
+                } else {
+                    List<ImageData> list = new ArrayList<>();
+                    list.add(imageData);
+                    groupData.put(group, list);
+                }
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
+
+        return groupData;
+    }
+
 }
