28b753b42fa9d5639391cd28f7589e7a22ecc037
오동규 <ehdrb700606@naver.com>
2024-11-09 19:46:17 +0900

    Feat: NFS 설정 smf파일 파싱 기능 추가, DefectDieArea 계산 삭제

--- integrated/src/main/java/com/semony/integrated/application/WaferServiceImpl.java
+++ integrated/src/main/java/com/semony/integrated/application/WaferServiceImpl.java
@@ -9,6 +9,7 @@ import com.semony.integrated.domain.dto.smf.WaferInspectionDTO;
 import com.semony.integrated.domain.dto.smf.WaferInspectionDTO.DefectRecord;
 import com.semony.integrated.domain.dto.smf.WaferInspectionSummaryDTO;
 import com.semony.integrated.domain.entity.EqpInspectionHstAlpha;
+import com.semony.integrated.domain.entity.FlowRecipe;
 import com.semony.integrated.domain.repository.EqpInspectionHstAlphaRepository;
 import com.semony.integrated.infrastructure.WaferInspectionFileReader;
 import java.io.IOException;
@@ -27,20 +28,26 @@ public class WaferServiceImpl implements WaferService {
 
     private final EqpInspectionHstAlphaRepository repository;
 
-    public SummaryWaferDto getWaferSummary(String lotId, BigDecimal lotSeq, String flowRecipe, String slotNo) {
+    public SummaryWaferDto getWaferSummary(String lotId, BigDecimal lotSeq, String flowRecipe,
+        String slotNo) {
 
-        List<EqpInspectionHstAlpha> list = repository.findByLotIdAndLotSeqAndFlowRecipeAndSlotNo(lotId, lotSeq, flowRecipe, slotNo);
+        List<EqpInspectionHstAlpha> list = repository.findByLotIdAndLotSeqAndFlowRecipeAndSlotNo(
+            lotId, lotSeq, flowRecipe, slotNo);
         return EqpInspectionHstAlphaConvertor.convert(list, flowRecipe);
     }
 
     @Override
-    public List<SummaryWaferDto> getWaferSummaryList(LocalDateTime startDate, LocalDateTime endDate) {
+    public List<SummaryWaferDto> getWaferSummaryList(LocalDateTime startDate,
+        LocalDateTime endDate) {
 
-        List<WaferSpecificationValueDto> inspectionHistory = repository.findInspectionHistory(startDate, endDate);
+        List<WaferSpecificationValueDto> inspectionHistory = repository.findInspectionHistory(
+            startDate, endDate);
+
         List<SummaryWaferDto> summaryWaferDtoList = new ArrayList<>();
 
         for (WaferSpecificationValueDto inspection : inspectionHistory) {
-            SummaryWaferDto waferSummary = getWaferSummary(inspection.getLotId(), inspection.getLotSeq(), inspection.getFlowRecipe(), inspection.getSlotNo());
+            SummaryWaferDto waferSummary = getWaferSummary(inspection.getLotId(),
+                inspection.getLotSeq(), inspection.getFlowRecipe(), inspection.getSlotNo());
             summaryWaferDtoList.add(waferSummary);
         }
 
@@ -48,23 +55,48 @@ public class WaferServiceImpl implements WaferService {
     }
 
     @Override
-    public WaferDetailDTO getWaferDetail(String lotId, BigDecimal lotSeq, String flowRecipe, String slotNo) throws IOException {
+    public WaferDetailDTO getWaferDetail(String lotId, BigDecimal lotSeq, String flowRecipe,
+        String slotNo) throws IOException {
 
         SummaryWaferDto waferSummary = getWaferSummary(lotId, lotSeq, flowRecipe, slotNo);
-
 
         WaferDetailDTO data = new WaferDetailDTO();
         data.setWaferInspections(new ArrayList<>());
 
-        Set<DiePos>[] sets = new HashSet[4];
-        for (int i = 1; i <= 3; i++) {
+        Set<DiePos>[] sets = new HashSet[3];
+        for (int i = 0; i < 3; i++) {
             sets[i] = new HashSet<>();
         }
 
-        // @Todo : 모듈 별 순서에 맞춰 읽어야 함.
-        for (int i = 1; i <= 3; i++) {
-            WaferInspectionDTO waferInspectionDTO = WaferInspectionFileReader.readFile(
-                "static/module" + i + ".smf");
+        PathFinder pathFinder = new PathFinder(
+            FlowRecipe.findByPpid(flowRecipe),
+            lotSeq.toString(),
+            lotId,
+            slotNo,
+            waferSummary.getDate()
+        );
+
+
+        for (int i = 0; i < 3; i++) {
+
+            String path = null;
+
+            switch (i){
+                case 0:
+                    path = pathFinder.getSmfPath(pathFinder.getInPath());
+                    break;
+                case 1:
+                    path = pathFinder.getSmfPath(pathFinder.getEwimPath());
+                    break;
+                case 2:
+                    path = pathFinder.getSmfPath(pathFinder.getOutPath());
+                    break;
+            }
+
+            WaferInspectionDTO waferInspectionDTO = WaferInspectionFileReader.readFile(path);
+
+
+
             data.setDieLocations(waferInspectionDTO.getDieLocations());
             waferInspectionDTO.setDieLocations(null);
 
@@ -82,69 +114,16 @@ public class WaferServiceImpl implements WaferService {
 
             waferInspectionDTO.setDefectCnt(defectCnt);
             waferInspectionDTO.setDefectDieCnt(sets[i].size());
-            waferInspectionDTO.setModuleId(waferSummary.getModules()[i-1].getModuleId());
-            waferInspectionDTO.setEventDtts(waferSummary.getModules()[i-1].getEventDtts());
+            waferInspectionDTO.setModuleId(waferSummary.getModules()[i].getModuleId());
+            waferInspectionDTO.setEventDtts(waferSummary.getModules()[i].getEventDtts());
 
             data.getWaferInspections().add(waferInspectionDTO);
 
         }
 
-        List<WaferInspectionSummaryDTO> calcWaferInspectionSummary = getCalcWaferInspectionSummary(sets);
-        data.setWaferInspectionSummaryDTO(calcWaferInspectionSummary);
 
         return data;
     }
 
-    private List<WaferInspectionSummaryDTO> getCalcWaferInspectionSummary(Set<DiePos>[] sets) {
-        List<WaferInspectionSummaryDTO> list = new ArrayList<>();
-
-        int dieSize = 1543;
-
-        // 1, 2, 3 선택 시 defectArea 계산
-        for (int i = 1; i <= 3; i++) {
-            Set<DiePos> set = sets[i];
-            WaferInspectionSummaryDTO dto = new WaferInspectionSummaryDTO();
-            dto.setDefectArea((double) set.size() / dieSize);
-            dto.setChecked(i);
-            list.add(dto);
-        }
-
-        // 12, 13, 23 선택 시 defectArea 계산
-        Set<DiePos> selectedSet = new HashSet<>();
-        selectedSet.addAll(sets[1]);
-        selectedSet.addAll(sets[2]);
-        WaferInspectionSummaryDTO dto = new WaferInspectionSummaryDTO();
-        dto.setDefectArea((double) selectedSet.size() / dieSize);
-        dto.setChecked(12);
-        list.add(dto);
-
-        selectedSet = new HashSet<>();
-        selectedSet.addAll(sets[1]);
-        selectedSet.addAll(sets[3]);
-        dto = new WaferInspectionSummaryDTO();
-        dto.setDefectArea((double) selectedSet.size() / dieSize);
-        dto.setChecked(13);
-        list.add(dto);
-
-        selectedSet = new HashSet<>();
-        selectedSet.addAll(sets[2]);
-        selectedSet.addAll(sets[3]);
-        dto = new WaferInspectionSummaryDTO();
-        dto.setDefectArea((double) selectedSet.size() / dieSize);
-        dto.setChecked(23);
-        list.add(dto);
-
-        // 123 선택 시 defectArea 개선
-        selectedSet = new HashSet<>();
-        selectedSet.addAll(sets[1]);
-        selectedSet.addAll(sets[2]);
-        selectedSet.addAll(sets[3]);
-        dto = new WaferInspectionSummaryDTO();
-        dto.setDefectArea((double) selectedSet.size() / dieSize);
-        dto.setChecked(123);
-        list.add(dto);
-        
-        return list;
-    }
 
 }
