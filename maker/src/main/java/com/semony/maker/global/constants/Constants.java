fa2984e64e16a5904049ff15ffb5f81cfb92c903
박지훈 <zlxlgus123@naver.com>
2024-11-11 09:19:58 +0900

    Feat : 모듈 상수화

--- maker/src/main/java/com/semony/maker/global/constants/Constants.java
+++ maker/src/main/java/com/semony/maker/global/constants/Constants.java
@@ -2,31 +2,24 @@ package com.semony.maker.global.constants;
 
 import java.util.List;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Value;
 
 public class Constants {
 
-    public static final Map<String, String> MODULE_IPS = Map.of(
-        "MIW7-51", "http://localhost:8000",
-        "MIW7-52", "http://localhost:8000",
-        "MIW7-61", "http://localhost:8002",
-        "MIW7-62", "http://localhost:8000",
-        "EWIM1-36", "http://localhost:8001",
-        "EWIM1-46", "http://localhost:8000",
-        "EWIM2-36", "http://localhost:8000",
-        "EWIM2-46", "http://localhost:8000"
-    );
+    @Value("#{${module_ips}}") // This will load the module_ips from the YAML
+    private Map<String, String> moduleIps;
 
     public static final String MODULE_DATA_URI = "/modules/data";
-    public static final String IN_FOLDER_PATH = "C:/root/in";
-    public static final String EWIM_FOLDER_PATH = "C:/root/ewim";
-    public static final String OUT_FOLDER_PATH = "C:/root/out";
+    public static final String IN_FOLDER_PATH = "/var/lib/result/root/in";
+    public static final String EWIM_FOLDER_PATH = "/var/lib/result/root/ewim";
+    public static final String OUT_FOLDER_PATH = "/var/lib/result/root/out";
     public static final String MACRO_INSPECTION = "Macro[Inspection]";
     public static final String EBR = "EBR";
     public static final String ERROR_INVALID_RECIPE_COMBINATION = "Invalid recipe combination";
     public static final String SUCCESS_MODULE_REQUEST = "Module request success";
     public static final String ERROR_MODULE_REQUEST = "Module request failed";
     public static final List<String> SUBFOLDER_OPTIONS = List.of("006", "010", "018", "022");
-    public static final int MAX_SLOT_COUNT  = 25;
+    public static final int MAX_SLOT_COUNT = 25;
 
     public static final String TIMESTAMP_PATTERN = "yyMMddHH";
 }
