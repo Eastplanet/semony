997a8767e5c8cab5c7619c33a24ade00722501ce
오동규 <ehdrb700606@naver.com>
2024-11-11 16:51:11 +0900

    Feat: 10초마다 핑을 보내서 연결이 정상적인지 확인

--- integrated/src/main/java/com/semony/integrated/IntegratedApplication.java
+++ integrated/src/main/java/com/semony/integrated/IntegratedApplication.java
@@ -2,8 +2,10 @@ package com.semony.integrated;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class IntegratedApplication {
 
 	public static void main(String[] args) {
