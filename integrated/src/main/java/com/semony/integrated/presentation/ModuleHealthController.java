ae9f0dae632df503cfd91df95da855bef00d5c19
오동규 <ehdrb700606@naver.com>
2024-11-11 16:51:26 +0900

    Feat: health를 체크할 수 있는 api

---
+++ integrated/src/main/java/com/semony/integrated/presentation/ModuleHealthController.java
@@ -0,0 +1,21 @@
+package com.semony.integrated.presentation;
+
+import com.semony.integrated.application.NetworkHealthChecker;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+@RequiredArgsConstructor
+public class ModuleHealthController {
+
+    private final NetworkHealthChecker networkHealthChecker;
+
+    @GetMapping()
+    public ResponseEntity<?> getModuleHealth() {
+        return ResponseEntity.ok(networkHealthChecker.getModuleHealth());
+    }
+}
