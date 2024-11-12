1a1002d735a4a2ae0e2cd268be3d8d85152ababf
박지훈 <zlxlgus123@naver.com>
2024-11-12 16:18:19 +0900

    Fix : 요청 형식 수정

--- module/main.py
+++ module/main.py
@@ -1,4 +1,6 @@
 import random
+from datetime import datetime
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from service import process_and_modify_in_module_data
@@ -17,25 +19,23 @@ class RequestModel(BaseModel):
   macroFolder: str
   selectedSubfolder: str
 
-
 @app.post("/modules/data")
 async def create_and_modify_macro_module_data(request: RequestModel):
   """
   로컬 폴더에서 IN 모듈 데이터를 복사하고 변조한 데이터를 동적으로 생성된 폴더에 저장하는 엔드포인트
   """
+  print(request)
   try:
-    # 하위 폴더를 랜덤 선택
-    # subfolder_options = ["006", "010", "018", "022"]
-    # selected_subfolder = random.choice(subfolder_options)
+    # request.date를 datetime 객체로 변환
+    request_time_dt = datetime.strptime(request.date, "%Y%m%dT%H:%M:%S")
 
-    # local_folder_path에 랜덤 하위 폴더 및 매크로 폴더 추가
-    # full_local_folder_path = f"{request.localFolderPath}/{request.selectedSubfolder}/{request.macroFolder}"
-    # print(request)
-    # print(full_local_folder_path)
-    # 파일 복사 및 변조 수행
+    # 원하는 형식으로 date를 변환
+    date = request_time_dt.strftime("%Y%m%d%H")
+
+    # IN 모듈 데이터 처리 및 변조
     defectImpo = await process_and_modify_in_module_data(
         request.moduleName,
-        request.date,
+        date,
         request.lotId,
         request.flowRecipe,
         request.lotSeq,
@@ -43,8 +43,9 @@ async def create_and_modify_macro_module_data(request: RequestModel):
         request.localFolderPath,
         request.macroFolder
     )
+    print(defectImpo)
     return defectImpo
-    # return {"status": "success", "data": response}
+
   except Exception as e:
     print(f"Error: {e}")  # 디버깅용 출력
     raise HTTPException(status_code=500, detail=str(e))
